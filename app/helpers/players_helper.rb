d4f836be2a71d7308ac79649de7b3f52f6864c7c
Adam Darowski <adarowski@patientslikeme.com>
2012-03-24 16:24:36 -0400

    Accounting for Ferrell and Ruth. Damn guys were good at everything.

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -11,11 +11,11 @@ module PlayersHelper
   PLAYER_SEASON_STAT_ATTRIBUTES = [
     "year", "age", "war_pos", "war_p", "war_tot",
     "wae", "wam",
+    "p_w", "p_l", "p_sv", "p_g", "p_gs", "p_cg", "p_sho", "p_ip",
+    "p_h", "p_r", "p_er", "p_hr", "p_bb", "p_so", "era",
     "b_g", "b_pa", "b_ab", "b_r", "b_h", "b_2b", "b_3b", "b_hr", "b_rbi",
     "b_bb", "b_so", "b_sb", "b_cs",
-    "ba", "obp", "slg", "ops",
-    "p_w", "p_l", "p_sv", "p_g", "p_gs", "p_cg", "p_sho", "p_ip",
-    "p_h", "p_r", "p_er", "p_hr", "p_bb", "p_so", "era"
+    "ba", "obp", "slg", "ops"
   ]
 
   def wwar_hall_count
